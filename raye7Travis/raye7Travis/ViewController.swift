e12b56af72e45b78b80e16610700967b0ae8664f
me9uid <me9uid@gmail.com>
2018-09-11 15:04:55 +0200

    add faild test

--- raye7Travis/raye7Travis/ViewController.swift
+++ raye7Travis/raye7Travis/ViewController.swift
@@ -15,8 +15,6 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
     
-    fawzy is stupid writing code outside of method
-
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
