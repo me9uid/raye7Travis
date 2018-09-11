647ed8a5c21365b9a613d3b62c8441247130f53c
me9uid <me9uid@gmail.com>
2018-09-11 15:01:45 +0200

    set destination simulator

--- raye7Travis/raye7Travis/ViewController.swift
+++ raye7Travis/raye7Travis/ViewController.swift
@@ -14,6 +14,8 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    fawzy is stupid writing code outside of method
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
